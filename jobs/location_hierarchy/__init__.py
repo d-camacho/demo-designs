f9fac04f1aa994e6cea3a029c0a4a79ad51801a8
Dwayne Camacho <dwayne.camacho1277@gmail.com>
2025-07-23 19:48:16 -0400

    Created Location Hierarchy Design files.

---
+++ jobs/location_hierarchy/__init__.py
@@ -0,0 +1,33 @@
+from nautobot.apps.jobs import DesignJob, register_jobs, IPNetworkVar, StringVar
+from nautobot.dcim.models import LocationType, Location
+from nautobot.ipam.models import Prefix, PrefixRole
+
+from .context import LocationHierarchyContext
+
+class LocationHierarchyDesign(DesignJob):
+    """Creates a hierarchy of location so users can nest new sites under."""
+    
+    region_name = StringVar(
+        label="Region",
+        description="Parent Region i.e. Americas, Europe, etc.",
+    )
+    country_name = StringVar(
+        label="Country",
+        description="Country where sites are located.",
+    )
+    region_prefix=IPNetworkVar(min_prefix_length=8, max_prefix_length=16)
+    country_prefix=IPNetworkVar(max_prefix_length=8, max_prefix_lenth=16)
+    has_sensitive_variables = False
+
+    class Meta:
+        """Pulls information from different files to render design."""
+        
+        name = "Location Hierarchy Design"
+        commit_default = False
+        description = "Creates nested location structure with assigned prefix."
+        design_file = "designs/hierarchy_design.yaml.j2"
+        context_class = LocationHierarchyContext
+        nautobot_version = ">=2"
+
+name = "Demo Designs"
+register_jobs(LocationHierarchyDesign)
---
+++ jobs/location_hierarchy/context/__init__.py
@@ -0,0 +1,37 @@
+from nautobot.extras.jobs import IPNetworkVar
+from nautobot.dcim.models import Location
+
+from nautobot_design_builder.context import Context
+from nautobot_design_builder.errors import DesignValidationError
+
+from netaddr import IPNetwork
+
+
+class LocationHierarchyContext(Context):
+    """Render context for Regions and nested Country"""
+
+    region_name: str
+    country_name: str
+    region_prefix: IPNetwork
+    
+
+    def validate_new_region_and_country(self):
+        """Ensure region and country don't already exist."""
+        # Check region
+        try:
+            region = Location.objects.get(name__iexact=self.region_name, location_type__name="Region")
+            raise DesignValidationError(f"A region already exists with the name '{self.region_name}'.")
+        except Location.DoesNotExist:
+            pass
+
+        # Check country under the region
+        try:
+            country = Location.objects.get(
+                name__iexact=self.country_name.strip(),
+                location_type__name="Country",
+                parent__name__iexact=self.region_name,
+            )
+            raise DesignValidationError(f"A country named '{self.country_name}' already exists under region '{self.region_name}'.")
+        except Location.DoesNotExist:
+            pass
+
