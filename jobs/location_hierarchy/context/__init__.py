bc23b2c29ae3526c932ccee7180bdc4d49647ee3
Dwayne Camacho <dwayne.camacho1277@gmail.com>
2025-07-24 15:17:52 -0400

    Changed validation to warning only to prevent job from failing.

--- jobs/location_hierarchy/context/__init__.py
+++ jobs/location_hierarchy/context/__init__.py
@@ -16,23 +16,21 @@ class LocationHierarchyContext(Context):
     region_prefix: IPNetwork
     country_prefix: IPNetwork
 
+    def validate(self):
+        self._validate_region()
+        self._validate_country()
+        self._validate_prefixes()
+
     def _validate_region(self):
-        try:
-            Location.objects.filter(name__iexact=self.region_name).exists()
-            raise DesignValidationError(f"Region '{self.region_name}' already exists.")
-        except Location.DoesNotExist:
-            return
+        if Location.objects.filter(name__iexact=self.region_name).exists():
+            self.logger.warning(f"Region '{self.region_name}' already exists.")
 
     def _validate_country(self):
-        try:
-            Location.objects.filter(name__iexact=self.country_name).exists()
-            raise DesignValidationError(f"Country '{self.country_name}' already exists.")
-        except Location.DoesNotExist:
-            return
-    
-    def _validate_prefix(self):
-        try:
-            Prefix.objects.filter(prefix=self.region_prefix).exists()
-            raise DesignValidationError(f"Prefix {self.region_prefix} already exists.")
-        except Prefix.DoesNotExist:
-            return 
+        if Location.objects.filter(name__iexact=self.country_name).exists():
+            self.logger.warning(f"Country '{self.country_name}' already exists.")
+
+    def _validate_prefixes(self):
+        if Prefix.objects.filter(prefix=self.region_prefix).exists():
+            self.logger.warning(f"Prefix {self.region_prefix} already exists.")
+        if Prefix.objects.filter(prefix=self.country_prefix).exists():
+            self.logger.warning(f"Prefix {self.country_prefix} already exists.")
