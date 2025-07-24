24566bc72a61d588407551280c1d75d59f66546f
Dwayne Camacho <dwayne.camacho1277@gmail.com>
2025-07-24 13:15:23 -0400

    Removed validation in LocationHierarchyContext.

--- jobs/location_hierarchy/context/__init__.py
+++ jobs/location_hierarchy/context/__init__.py
@@ -16,23 +16,23 @@ class LocationHierarchyContext(Context):
     country_prefix: IPNetwork
     
 
-    def validate_new_region_and_country(self):
-        """Ensure region and country don't already exist."""
-        # Check region
-        try:
-            region = Location.objects.get(name__iexact=self.region_name, location_type__name="Region")
-            raise DesignValidationError(f"A region already exists with the name '{self.region_name}'.")
-        except Location.DoesNotExist:
-            pass
+    # def validate_new_region_and_country(self):
+    #     """Ensure region and country don't already exist."""
+    #     # Check region
+    #     try:
+    #         region = Location.objects.get(name__iexact=self.region_name, location_type__name="Region")
+    #         raise DesignValidationError(f"A region already exists with the name '{self.region_name}'.")
+    #     except Location.DoesNotExist:
+    #         pass
 
-        # Check country under the region
-        try:
-            country = Location.objects.get(
-                name__iexact=self.country_name.strip(),
-                location_type__name="Country",
-                parent__name__iexact=self.region_name,
-            )
-            raise DesignValidationError(f"A country named '{self.country_name}' already exists under region '{self.region_name}'.")
-        except Location.DoesNotExist:
-            pass
+    #     # Check country under the region
+    #     try:
+    #         country = Location.objects.get(
+    #             name__iexact=self.country_name.strip(),
+    #             location_type__name="Country",
+    #             parent__name__iexact=self.region_name,
+    #         )
+    #         raise DesignValidationError(f"A country named '{self.country_name}' already exists under region '{self.region_name}'.")
+    #     except Location.DoesNotExist:
+    #         pass
 
