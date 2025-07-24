cda5f209d9bd0e2aa33f8078d7ad31987492f13e
Dwayne Camacho <dwayne.camacho1277@gmail.com>
2025-07-24 13:18:54 -0400

    Updated LocationHierarchyDesign input fields.

--- jobs/location_hierarchy/__init__.py
+++ jobs/location_hierarchy/__init__.py
@@ -5,18 +5,28 @@ from nautobot_design_builder.design_job import DesignJob
 from .context import LocationHierarchyContext
 
 class LocationHierarchyDesign(DesignJob):
-    """Creates a hierarchy of location so users can nest new sites under."""
-    
+    """Creates a hierarchy of locations so users can nest new sites under."""
+
     region_name = StringVar(
         label="Region",
-        description="Parent Region i.e. Americas, Europe, etc.",
+        description="Parent Region (e.g., Americas, Europe, etc.)",
     )
     country_name = StringVar(
         label="Country",
-        description="Country where sites are located.",
+        description="Country where sites will be located (e.g., United States)",
     )
-    region_prefix=IPNetworkVar(min_prefix_length=8, max_prefix_length=16)
-    country_prefix=IPNetworkVar(min_prefix_length=8, max_prefix_length=16)
+    region_prefix = IPNetworkVar(
+        label="Region Prefix",
+        min_prefix_length=8,
+        max_prefix_length=16,
+        description="Top-level container prefix (e.g., 10.0.0.0/8)",
+    )
+    country_prefix = IPNetworkVar(
+        label="Country Prefix",
+        min_prefix_length=8,
+        max_prefix_length=16,
+        description="Child container prefix for the country (e.g., 10.1.0.0/12)",
+    )
     has_sensitive_variables = False
 
     class Meta:
@@ -24,7 +34,7 @@ class LocationHierarchyDesign(DesignJob):
         
         name = "Location Hierarchy Design"
         commit_default = False
-        description = "Creates nested location structure with assigned prefix."
+        description = "Creates nested location structure with assigned container prefix."
         design_file = "designs/hierarchy_design.yaml.j2"
         context_class = LocationHierarchyContext
         nautobot_version = ">=2"
